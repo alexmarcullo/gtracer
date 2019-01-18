b0c3b0c69dc78a8e3e7da4b4588d26769a72320b
Alex Gonçalves <agoncalves@globosat.com.br>
2019-01-18 18:11:50 -0200

    Adicionando teste do appdynamics

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const express = require('express');
 const tracer = require('./routes/tracer');
 const app = express();
 
+require("appdynamics").profile({
+    controllerHostName: 'cloud2019011711580214.saas.appdynamics.com',
+    controllerPort: 443, 
+
+    // If SSL, be sure to enable the next line
+    controllerSslEnabled: false,
+    accountName: 'cloud2019011711580214',
+    accountAccessKey: 'nbal2rcil57e',
+    applicationName: '.NET',
+    tierName: 'Node',
+    nodeName: 'process' // The controller will automatically append the node name with a unique number
+});
+   
+
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
